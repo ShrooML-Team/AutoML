7621e6ef15d59ba7cf3615f0b60ef0d30df67a43
meo <meo.prn@outlook.fr>
2026-02-10 14:16:24 +0100

    fix: corriger l'URL de la base de données et mettre à jour la configuration de docker-compose

--- api/db.py
+++ api/db.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = "sqlite:////app/db/users.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
